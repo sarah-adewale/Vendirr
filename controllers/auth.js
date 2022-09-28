0f2c17ee8d3f23ace50b8b1dad5023a29c5225f8
Sarah Adewale <saraphynaadewale@gmail.com>
2022-09-28 01:00:19 +0100

    finished login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,17 +44,17 @@ exports.postLogin = (req, res, next) => {
   })(req, res, next);
 };
 
-// exports.logout = (req, res) => {
-//   req.logout(() => {
-//     console.log('User has logged out.')
-//   })
-//   req.session.destroy((err) => {
-//     if (err)
-//       console.log("Error : Failed to destroy the session during logout.", err);
-//     req.user = null;
-//     res.redirect("/");
-//   });
-// };
+exports.logout = (req, res) => {
+  req.logout(() => {
+    console.log('User has logged out.')
+  })
+  req.session.destroy((err) => {
+    if (err)
+      console.log("Error : Failed to destroy the session during logout.", err);
+    req.user = null;
+    res.redirect("/");
+  });
+};
 
 exports.getSignup = (req, res) => {
   if (req.user) {
@@ -92,7 +92,7 @@ exports.postSignup = (req, res, next) => {
   });
 
   User.findOne(
-    { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
+    { $or: [{ email: req.body.email }, { firstName: req.body.firstName }, { lastName: req.body.lastName }] },
     (err, existingUser) => {
       if (err) {
         return next(err);
---
+++ models/Post.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  vendorName: {
+    type: String,
+    required: true,
+  },
+  tags:{
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  cloudinaryId: {
+    type: String,
+    require: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  reviews: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
