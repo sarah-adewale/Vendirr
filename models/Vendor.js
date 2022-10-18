54d34cce1581b9ffca6ec629491c20ed8b42c1f5
Sarah Adewale <saraphynaadewale@gmail.com>
2022-10-18 15:50:48 +0100

    completed login and signup routes

--- models/Vendor.js
+++ models/Vendor.js
@@ -1,43 +1,51 @@
-const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
 const VendorSchema = new mongoose.Schema({
-  firstName: { type: String, unique: true },
-  lastName: { type: String, unique: true },
-  email: { type: String, unique: true },
-  password: String,
+  vendorName: {
+    type: String,
+    required: true,
+  },
+  // tags:{
+  //   type: String,
+  //   required: true,
+  // },
+  image: {
+    type: String,
+    require: true,
+  },
+  cloudinaryId: {
+    type: String,
+    require: true,
+  },
+  about: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  closingTime: {
+    type: String,
+    required: true,
+  },
+  websiteLink: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+  
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
-
-// Password hash middleware.
-
-VendorSchema.pre("save", function save(next) {
-  const vendor = this;
-  if (!vendor.isModified("password")) {
-    return next();
-  }
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) {
-      return next(err);
-    }
-    bcrypt.hash(vendor.password, salt, (err, hash) => {
-      if (err) {
-        return next(err);
-      }
-      vendor.password = hash;
-      next();
-    });
-  });
-});
-
-// Helper method for validating user's password.
-
-VendorSchema.methods.comparePassword = function comparePassword(
-  candidatePassword,
-  cb
-) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    cb(err, isMatch);
-  });
-};
 
 module.exports = mongoose.model("Vendor", VendorSchema);
--- routes/main.js
+++ routes/main.js
@@ -2,26 +2,23 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth') 
 const homeController = require('../controllers/home')
-const postsController = require('../controllers/posts')
+const vendorsController = require('../controllers/vendor')
 const exploreController = require('../controllers/explore')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', homeController.getIndex)
-router.get('/explore', exploreController.getExplore)
-router.get('/profile', ensureAuth, postsController.getProfile)
-router.get('/businessprofile', ensureAuth, postsController.getBusinessprofile)
-router.get('/security', ensureAuth, postsController.getSecurity)
-router.get('/vendorprofile', ensureAuth, postsController.getVendorprofile)
-router.get('/feed', ensureAuth, postsController.getFeed)
+router.get('/explore', ensureAuth, exploreController.getExplore)
+router.get('/profile', ensureAuth, vendorsController.getProfile)
+router.get('/businessprofile', ensureAuth, vendorsController.getBusinessprofile)
+router.get('/security', ensureAuth, vendorsController.getSecurity)
+
+// auth controller
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
-// router.get('/vendorlogin', authController.getVendorlogin)
-// router.post('/vendorlogin', authController.postVendorlogin)
-// router.get("/explore", ensureAuth, postsController.getProfile);
 router.get('/logout', authController.logout) 
 router.get('/signup', authController.getSignup)
 router.post('/signup', authController.postSignup)
-// router.get('/vendorsignup', authController.getVendorsignup)
-// router.post('/vendorsignup', authController.postVendorsignup)
+router.get('/vendorsignup', vendorsController.getVendorsignup)
+
 
 module.exports = router
